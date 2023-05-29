b11b24e76b276b3d500b91b811e6ea5906867915
Elam Day-Friedland <elamdf@eecs.berkeley.edu>
2023-05-29 13:40:09 -0700

    add note about fonts

--- lua/elamdf/init.lua
+++ lua/elamdf/init.lua
@@ -1,3 +1,4 @@
+# NOTE: you need to install a nerd font in interm2 for thigns to wkr, I use hack nerd font # brew tap homebrew/cask-fonts brew install --cask font-hack-nerd-font
 print("how do you do, bestie? :D")
 require("elamdf.set")
 require("elamdf.remap")
